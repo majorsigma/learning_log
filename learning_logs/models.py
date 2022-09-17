ff49725f116685b2ea6ce49cf6e53e79b1f7fe0c
Olalekan Ogundele <olalekan.o.ogundele@gmail.com>
2022-09-17 05:49:38 +0100

    Give the project some Bootstrap styling

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -19,9 +19,14 @@ class Entry(models.Model):
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
+
     class Meta:
         verbose_name_plural = 'entries'
 
     def __str__(self):
         """Return a string representation of the model."""
         return f"{self.text[:50]}..."
+
+    
+    
+    
